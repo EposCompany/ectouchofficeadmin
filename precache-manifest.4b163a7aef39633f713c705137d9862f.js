845e29aa42906df71b1991f6626c9a85b1c1904f
EposCompany <sales@eposcompany.co.uk>
2021-10-23 12:19:43 +0100

    Updates

---
+++ precache-manifest.4b163a7aef39633f713c705137d9862f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb2d3638c3e6d8a538fac81439cda7d",
+    "url": "/EposCompany/ectouchofficeadmin.git/index.html"
+  },
+  {
+    "revision": "13de0ca9598571fe6a8d",
+    "url": "/EposCompany/ectouchofficeadmin.git/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "c8a9d16a279d96ca4524",
+    "url": "/EposCompany/ectouchofficeadmin.git/static/js/2.87337f43.chunk.js"
+  },
+  {
+    "revision": "63f309877853efb81650b071b20ca45c",
+    "url": "/EposCompany/ectouchofficeadmin.git/static/js/2.87337f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13de0ca9598571fe6a8d",
+    "url": "/EposCompany/ectouchofficeadmin.git/static/js/main.da0c638e.chunk.js"
+  },
+  {
+    "revision": "2383728c1a2167b7eb97",
+    "url": "/EposCompany/ectouchofficeadmin.git/static/js/runtime-main.e2be1803.js"
+  }
+]);
