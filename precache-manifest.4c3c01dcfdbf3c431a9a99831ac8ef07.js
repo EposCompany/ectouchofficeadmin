2e8d5d1837fe4b981f420672dbad4fc0f093dd65
EposCompany <sales@eposcompany.co.uk>
2021-10-29 15:41:38 +0100

    Updates

---
+++ precache-manifest.4c3c01dcfdbf3c431a9a99831ac8ef07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960617cfea1b31ff6e466e0322d01069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f185528f355298380a",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "c8a9d16a279d96ca4524",
+    "url": "/static/js/2.87337f43.chunk.js"
+  },
+  {
+    "revision": "63f309877853efb81650b071b20ca45c",
+    "url": "/static/js/2.87337f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f185528f355298380a",
+    "url": "/static/js/main.8b9a6990.chunk.js"
+  },
+  {
+    "revision": "e767c41f02ff59ab7e4f",
+    "url": "/static/js/runtime-main.e5c7adbd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ectouchofficeadmin/precache-manifest.b532f359d68461d93769bb074611d634.js"
+  "/precache-manifest.4c3c01dcfdbf3c431a9a99831ac8ef07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ectouchofficeadmin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
